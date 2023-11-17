7a8cc4d5a440f3e2d568131b02fa6c2053d72937
Your Name <you@example.com>
2023-11-16 20:35:40 -0500

    servidor nodejs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //Obtener los productos de la API
 async function ObtenerProductos() {
     try {
-        const response = await fetch('https://fakestoreapi.com/products');
+        const response = await fetch('https://fakestoreapi.juan-esteban644.repl.co/products'); //API CREADA
         if (!response.ok) { // Verificar si la respuesta no está ok
             throw new Error('Error al obtener productos');
         }
@@ -17,26 +17,24 @@ async function ObtenerProductos() {
 //CARDS
 document.addEventListener("DOMContentLoaded", async () => {
     const contenedor = document.querySelector(".contenedor");
-    const products = await ObtenerProductos(); // Esperar a que la función ObtenerProductos termine
+    const prods = await ObtenerProductos(); // Esperar a que la función ObtenerProductos termine
 
-    if (products.length > 0) {
-        let productos = "";
-        products.forEach(product => {
-            localStorage.setItem(`product_${product.id}`, JSON.stringify(product));
-            productos += `
-                <div class="card" style="width: 18rem;">
-                    <img src="${product.image}" class="card-img-top">
-                    <div class="card-body">
-                        <h3 class="card-title">${product.title}</h3>
-                        <p class="card-text">${product.description}</p>
-                        <h2>${product.price}</h2>
-                        <a href="/detalleProducto.html?id=${product.id}" class="btn btn-primary">Ver detalle</a>
-                    </div>
+    let productos = "";
+    prods.test.forEach(product => {
+        localStorage.setItem(`product_${product.id}`, JSON.stringify(product));
+        productos += `
+            <div class="card" style="width: 18rem;">
+                <img src="${product.image}" class="card-img-top">
+                <div class="card-body">
+                    <h3 class="card-title">${product.title}</h3>
+                    <p class="card-text">${product.description}</p>
+                    <h2>${product.price}</h2>
+                    <a href="/detalleProducto.html?id=${product.id}" class="btn btn-primary">Ver detalle</a>
                 </div>
-            `;
-        });
-        contenedor.innerHTML = productos;
-    } else {
-        contenedor.innerHTML = "No se encontraron productos.";
-    }
+            </div>
+        `;
+    });
+    
+    contenedor.innerHTML = productos;
+    
 });
